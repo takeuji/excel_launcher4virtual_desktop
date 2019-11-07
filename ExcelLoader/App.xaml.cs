0208c2304c09f246dc88950b007fa3b0494e771e
Daisuke Takeuji <takeuji@gmail.com>
2019-11-07 17:07:35 +0900

    add base code

---
+++ ExcelLoader/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows;
+
+namespace ExcelLoader
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            //App app = new App();
+            //app.InitializeComponent();
+            //app.Run();
+
+            try
+            {
+                var commandLine = ConfigurationManager.AppSettings["Application"];
+                if (!String.IsNullOrEmpty(commandLine))
+                {
+                    Process proc = new Process();
+                    proc.StartInfo.FileName = commandLine;
+                    proc.StartInfo.Arguments = " -x";
+                    proc.Start();
+                }
+                System.Diagnostics.Debug.WriteLine("command line : " + commandLine);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
